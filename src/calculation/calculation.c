14d02529d4e6f7683cca51f4b6b3a31f086d0008
Jay R Galindez <jay.r.galindez.mil@army.mil>
2022-08-31 09:42:10 -0400

    [Modified calculation.c] Creating the initial equation structure

--- src/calculation/calculation.c
+++ src/calculation/calculation.c
@@ -1,7 +1,32 @@
 #include <calculation.h>
-#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
 
-void print_it(void)
+
+
+typedef enum eq_result_t
 {
-    printf("HI!\n");
+    EQ_SOLVED,     // Equation was successfully solved
+    EQ_FAILURE,    // Equation resulted in a failure
+    EQ_UNSOLVED    // Equation has not been attempted yet
+
+} eq_result_t;
+
+typedef struct equation_t
+{
+    uint64_t l_operand;
+    uint64_t r_operand;
+    union
+    {
+        uint64_t u_evaluation;
+        int64_t s_evaluation;
+    } evaluation;
+    uint8_t operator;
+    char * error_msg;
+} equation_t;
+
+equation_t * get_equation_struct(uint64_t l_operand, uint8_t operator, uint64_t r_operand)
+{
+    equation_t * equation = (equation_t *)malloc(sizeof(equation_t));
+
 }
