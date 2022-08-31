bd1a3bff42b8f7f36808553a6a97160aef6be719
Jay R Galindez <jay.r.galindez.mil@army.mil>
2022-08-31 13:42:44 -0400

    [Added addition test] Additions are tested and passing

--- include/calculation.h
+++ include/calculation.h
@@ -7,6 +7,13 @@ extern "C" {
 
 #include <stdint.h>
 #include <utils.h>
+// This enum is used for the eval union return value
+typedef enum eq_eval_type_t
+{
+    EQ_VAL_SIGNED,      // Eval result is of type signed
+    EQ_VAL_UNSIGNED     // Eval result is of type unsigned
+} eq_eval_type_t;
+
 typedef enum eq_result_t
 {
     EQ_SOLVED,     // Equation was successfully solved
@@ -14,9 +21,23 @@ typedef enum eq_result_t
     EQ_UNSOLVED    // Equation has not been attempted yet
 } eq_result_t;
 
+// Structure contains the data needed to resolve the equation
+typedef struct equation_t
+{
+    uint32_t eq_id;         // Equation ID provided by the spec
+    char * error_msg;       // Message indicating the error
+    uint64_t l_operand;     // Left operand
+    uint64_t r_operand;     // Right operand
+    uint64_t evaluation;    // Eval result
+    eq_eval_type_t sign;    // Whether the result is singed or unsigned
+    eq_result_t result;     // Conclusion of the operation
+    uint8_t opt;            // Operator byte code
+} equation_t;
 
-typedef struct equation_t equation_t;
-equation_t * get_equation_struct(uint64_t l_operand, uint8_t opt, uint64_t r_operand);
+equation_t * get_equation_struct(uint32_t equation_id,
+                                 uint64_t l_operand,
+                                 uint8_t opt,
+                                 uint64_t r_operand);
 void free_equation_struct(equation_t * equation);
 
 #ifdef __cplusplus
--- tests/gtest_calculation.cpp
+++ tests/gtest_calculation.cpp
@@ -1,9 +1,80 @@
 #include <gtest/gtest.h>
 #include <calculation.h>
 
+struct hexchar
+{
+    char c;
+};
+
+std::ostream& operator<<(std::ostream& os, hexchar h)
+{
+    auto f = os.flags();
+    os << "0x";
+    os << std::setw(2) << std::setfill('0') << std::hex << std::uppercase;
+    os << (unsigned int)(unsigned char)h.c;
+    os.flags(f);
+    return os;
+}
+
 TEST(TestAllocs, TestAllocs)
 {
-    equation_t * eq = get_equation_struct(10, 0x01, 30);
+    equation_t * eq = get_equation_struct(0, 10, 0x01, 30);
+
     EXPECT_NE(eq, nullptr);
     free_equation_struct(eq);
 }
+
+class CalculationFixture :public ::testing::TestWithParam<std::tuple<uint64_t, uint8_t, uint64_t, int, bool>>{};
+
+TEST_P(CalculationFixture, TestingCalculations)
+{
+    auto [l_opr, opt, r_optr, expected_int, expected_succ] = GetParam();
+    equation_t * eq = get_equation_struct(0, l_opr, opt, r_optr);
+
+    if (1 == expected_succ)
+    {
+        EXPECT_EQ(eq->result, EQ_SOLVED);
+        EXPECT_EQ(eq->evaluation, expected_int) << l_opr << " (" << hexchar{opt} << ") " << r_optr << " = " << expected_int;
+    }
+    else
+    {
+        EXPECT_EQ(eq->result, EQ_FAILURE);
+    }
+    free_equation_struct(eq);
+}
+
+/*
+ * Param Order
+ * l_operand
+ * opt
+ * r_operand
+ * expected_result
+ * expected_err
+ */
+INSTANTIATE_TEST_SUITE_P(
+        AdditionTests,
+        CalculationFixture,
+        ::testing::Values(
+            std::make_tuple(10, 0x01, 10, 20, 1),
+            std::make_tuple(INT64_MAX, 0x01, 10, 0, 0),
+            std::make_tuple(INT64_MIN, 0x01, -10, 0, 0),
+            std::make_tuple(1000, 0x01, 1000, 2000, 1)
+        ));
+
+/*
+ * Param Order
+ * l_operand
+ * opt
+ * r_operand
+ * expected_result
+ * expected_err
+ */
+INSTANTIATE_TEST_SUITE_P(
+    SubtractionTest,
+    CalculationFixture,
+    ::testing::Values(
+        std::make_tuple(10, 0x01, 10, 20, 1),
+        std::make_tuple(INT64_MAX, 0x01, 10, 0, 0),
+        std::make_tuple(INT64_MIN, 0x01, -10, 0, 0),
+        std::make_tuple(1000, 0x01, 1000, 2000, 1)
+    ));
