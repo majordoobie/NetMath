4dfb5526ec0e3ea99036406ac9e51b4da4ca12c3
Jay R Galindez <jay.r.galindez.mil@army.mil>
2022-09-01 13:57:37 -0400

    [Modified gtest_header_parser.cpp] Created a function for finding the files, since they change depending on how the test are running

--- tests/gtest_header_parser.cpp
+++ tests/gtest_header_parser.cpp
@@ -1,6 +1,6 @@
 #include <gtest/gtest.h>
 #include <header_parser.h>
-#include <string.h>
+#include <fcntl.h>
 
 
 int possible_paths = 3;
@@ -8,7 +8,7 @@ const char * base_paths[3] = {"tests/equ_files/",
                               "../../tests/equ_files/",
                               "../../../../tests/equ_files/"};
 
-FILE * get_file(const char * file_name)
+int get_file(const char * file_name)
 {
     for (int i = 0; i < possible_paths; i++)
     {
@@ -16,19 +16,23 @@ FILE * get_file(const char * file_name)
         char full_path[NAME_MAX];
         snprintf(full_path, NAME_MAX, "%s%s", base_paths[i], file_name);
 
-        FILE * file = fopen(full_path, "rb");
-        if (NULL != file)
+        int file = open(full_path, O_CLOEXEC | O_RDONLY);
+        if (-1 != file)
         {
             return file;
         }
     }
-    fprintf(stderr, "Unable to find file %s\n", file_name);
-    return NULL;
+    return -1;
 }
 
 TEST(Simple, Simple)
 {
-    FILE * file = get_file("3a78786203e77c0b.equ");
-    EXPECT_NE(file, nullptr);
-    fclose(file);
+    const char * file_name = "valid_3a78786203e77c0b.equ";
+    int file = get_file(file_name);
+    ASSERT_NE(file, -1) << "[!] File provided " << file_name << " was not found\n";
+
+    uint32_t magic_filed = 0;
+    read(file, &magic_filed, HEAD_MAGIC);
+    EXPECT_EQ(magic_filed, MAGIC_VALUE);
+    close(file);
 }
