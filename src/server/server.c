07484f5b29987c473a9e60f1d89a3c849b87c464
Jay R Galindez <jay.r.galindez.mil@army.mil>
2022-09-12 11:32:32 -0400

    [Added base arg tester to gtest_server.cpp] Created the base test function to test all the parameters in c

--- src/server/server.c
+++ src/server/server.c
@@ -1,2 +1,51 @@
 #include <server.h>
+#include <stdint.h>
+#include <stdlib.h>
 
+// Enable private function to be used publicly when in debug release mode
+#ifdef NDEBUG
+#define DEBUG_STATIC static
+#else
+#define DEBUG_STATIC
+#endif
+
+typedef enum
+{
+    DEFAULT_PORT    = 31337,
+    DEFAULT_THREADS = 4
+} args_default_t;
+
+typedef struct args_t
+{
+    uint32_t port;
+    uint8_t threads;
+} args_t;
+
+DEBUG_STATIC args_t * parse_args(int argc, char ** argv);
+DEBUG_STATIC void free_args(args_t * args);
+
+DEBUG_STATIC void free_args(args_t * args)
+{
+    free(args);
+}
+
+DEBUG_STATIC args_t * parse_args(int argc, char ** argv)
+{
+    args_t * args = (args_t *)malloc(sizeof(args_t));
+    if (UV_INVALID_ALLOC == verify_alloc(args))
+    {
+        return NULL;
+    }
+    *args = (args_t){
+        .port       = DEFAULT_PORT,
+        .threads    = DEFAULT_THREADS
+    };
+
+    // If not additional arguments have been specified, return the default;
+    if (1 == argc)
+    {
+        return args;
+    }
+
+    return NULL;
+}
--- tests/gtest_server.cpp
+++ tests/gtest_server.cpp
@@ -1,6 +1,65 @@
 #include <gtest/gtest.h>
+#include <server.h>
+
+extern "C"
+{
+    typedef struct args_t
+    {
+        uint32_t port;
+        uint8_t threads;
+    } args_t;
+    args_t * parse_args(int argc, char ** argv);
+    void free_args(args_t * args);
+}
+
+class ServerCmdTester : public ::testing::TestWithParam<std::tuple<std::vector<std::string>, bool>>{};
+
+// Parameter test handles signed testing
+TEST_P(ServerCmdTester, TestingServerArguments)
+{
+    auto [str_vector, expect_failure] = GetParam();
+    int arg_count = (int)str_vector.size();
+
+    char ** arr = new char*[str_vector.size()];
+    for(size_t i = 0; i < str_vector.size(); i++)
+    {
+        arr[i] = new char[str_vector[i].size() + 1];
+        strcpy(arr[i], str_vector[i].c_str());
+    }
+
+    args_t * args = parse_args(arg_count, arr);
+    if (expect_failure)
+    {
+        EXPECT_EQ(args, nullptr);
+    }
+    else
+    {
+        EXPECT_NE(args, nullptr);
+    }
+
+
+    for(size_t i = 0; i < str_vector.size(); i++)
+    {
+        delete [] arr[i];
+    }
+    delete [] arr;
+
+    free_args(args);
+}
+
+
+INSTANTIATE_TEST_SUITE_P(
+    AdditionTests,
+    ServerCmdTester,
+    ::testing::Values(
+        std::make_tuple(std::vector<std::string>{__FILE__}, false)
+    ));
+
 
 TEST(One, One)
 {
-    EXPECT_EQ(1, 1);
+    args_t * args = parse_args(1, (char **)"1");
+    EXPECT_NE(nullptr, args);
+    free_args(args);
+
 }
