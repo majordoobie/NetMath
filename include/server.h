461509822954e9268400b0c87ce67dd317ed31b4
Jay R Galindez <jay.r.galindez.mil@army.mil>
2022-09-12 10:20:23 -0400

    [Created server.c] server.c initialized

---
+++ include/server.h
@@ -0,0 +1,15 @@
+#ifndef JG_NETCALC_SRC_SERVER_SERVER_H_
+#define JG_NETCALC_SRC_SERVER_SERVER_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif //END __cplusplus
+
+#include <utils.h>
+
+
+#ifdef __cplusplus
+}
+#endif // END __cplusplus
+
+#endif //JG_NETCALC_SRC_SERVER_SERVER_H_
---
+++ tests/gtest_server.cpp
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+
+TEST(One, One)
+{
+    EXPECT_EQ(1, 1);
+}
